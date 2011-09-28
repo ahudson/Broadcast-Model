29f219d477928369c07f210a8a8a59ac6baafe4a
Austin Hudson <ahudson120@gmail.com>
2011-09-28 12:59:11 -0500

    first commit

---
+++ BroadcastServerDriver/BroadcastServerDriver/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BroadcastServer;
+
+namespace BroadcastServerDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BroadcastServer.BroadcastServer server = new BroadcastServer.BroadcastServer("input.txt");
+            server.sendFile();
+        }
+    }
+}
